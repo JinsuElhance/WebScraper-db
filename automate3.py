ce6f319f05afafadc884bbc356b0ce3fbe7825c5
JinsuElhance <33761390+JinsuElhance@users.noreply.github.com>
2018-11-18 17:30:15 -0800

    Added show_table method, inserted into table.

--- automate3.py
+++ automate3.py
@@ -1,9 +1,19 @@
 import sqlite3
+import pandas as pd
+
 conn = sqlite3.connect('automa_job.db')
+
+conn = sqlite3.connect("WebScraper-db\web.db")
 
 mydb = conn.cursor()
 mydb.execute(''' CREATE TABLE jobs
-				(title text, company text, link text, term text)''')
+    (title text, company text, link text, term text)''')
 mydb.execute("INSERT INTO jobs VALUES ('semi-professional waffle cosplayer','Office of Carol Christ','pornhub.com','20XX-20YY')")
 conn.commit()
+
+def show_table(table):
+	sql_statement = "SELECT * FROM ?", table
+	pd.read_sql(sql_statement, conn)
+conn.commit()
+
 conn.close()
